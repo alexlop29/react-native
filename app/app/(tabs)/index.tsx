fead27bfcf0265eeafff106eeccc96657afa5c06
alexlop29 <alexander.lopez@owasp.org>
2024-10-22 13:26:11 -0400

    Save point

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,39 +1,16 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { styled } from "nativewind";
+
+const StyledView = styled(View);
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-    </ParallaxScrollView>
+    <StyledView>
+      {/* Stat Viewer */}
+      <StyledView>
+
+      </StyledView>
+    </StyledView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
