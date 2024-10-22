f5b8bb27ce856145fff32106775d8f170616ab5d
alexlop29 <alexander.lopez@owasp.org>
2024-10-22 19:44:42 -0400

    Init native wind

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -5,7 +5,11 @@ import { Link } from "expo-router";
 export default function HomeScreen() {
   return (
       <View>
-        <Link href="../workout">Start Training</Link>
+        <View></View>
+        <View>
+          <Link href="../workout">Start Training</Link>
+        </View>
+        <View></View>
       </View>
   );
 }
