540b0a807bb27b8ec650018113f0563d45ec95e5
alexlop29 <alexander.lopez@owasp.org>
2024-10-22 20:08:53 -0400

    Configure native wind

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./app/components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/(tabs)/**/*.{js,jsx,ts,tsx}", "./app/components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
