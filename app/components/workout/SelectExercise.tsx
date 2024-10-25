e3d7afdaa37a5e3494a8bc9d3a87e57067af3f6f
alexlop29 <alexander.lopez@owasp.org>
2024-10-25 19:56:17 -0400

    Add lucide-react-native icon

--- app/components/workout/SelectExercise.tsx
+++ app/components/workout/SelectExercise.tsx
@@ -1,21 +1,59 @@
+// core
 import { View } from "react-native";
 import { useState } from "react";
 
-const SelectExercise = () => {
-    // may be able to use prop to associate back to the parent workout!
+// comps
+import {
+  Select,
+  SelectTrigger,
+  SelectInput,
+  SelectIcon,
+  SelectPortal,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicatorWrapper,
+  SelectDragIndicator,
+  SelectItem,
+} from "@/components/ui/select";
+import { Icon } from "@/components/ui/icon";
 
-    // can use react query to retrieve list of exercises from database in firebase cloud storage
-    const [ exercise, setExercise ] = useState("");
+const SelectExercise = () => {
+  // may be able to use prop to associate back to the parent workout!
+
+  // can use react query to retrieve list of exercises from database in firebase cloud storage
+  const [exercise, setExercise] = useState("");
 
   return (
     <View>
-        {/* Should add form component to simplify submit */}
-        {/* Need to add drop down */}
-        {/* Will need option to add a new exercise */}
+      {/* Need to add drop down */}
+      {/* Will need option to add a new exercise */}
+      <Select>
+        <SelectTrigger variant="outline" size="md">
+          <SelectInput placeholder="Select option" />
+          <SelectIcon className="mr-3" as={ChevronDownIcon} />
+          <Icon as={EditIcoChn} size="md" />
+        </SelectTrigger>
+        <SelectPortal>
+          <SelectBackdrop />
+          <SelectContent>
+            <SelectDragIndicatorWrapper>
+              <SelectDragIndicator />
+            </SelectDragIndicatorWrapper>
+            <SelectItem label="UX Research" value="ux" />
+            <SelectItem label="Web Development" value="web" />
+            <SelectItem
+              label="Cross Platform Development Process"
+              value="Cross Platform Development Process"
+            />
+            <SelectItem label="UI Designing" value="ui" isDisabled={true} />
+            <SelectItem label="Backend Development" value="backend" />
+          </SelectContent>
+        </SelectPortal>
+      </Select>
     </View>
-  )
-}
+  );
+};
 
 // Should handle new set in a different component
 
-export default SelectExercise
+export default SelectExercise;
