faf3bc24e50ad44e7887e8dafefa3bcac880f781
alexlop29 <alexander.lopez@owasp.org>
2024-11-01 16:39:14 -0400

    Add dialog to allow the user to select an exercise and add a set to the workout

--- app/app/workout/[id].tsx
+++ app/app/workout/[id].tsx
@@ -6,6 +6,15 @@ import { router } from "expo-router";
 // comps
 import { Input, InputField } from "@/components/ui/input";
 import { Button, ButtonText } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogBackdrop,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+} from "@/components/ui/alert-dialog";
+import { Heading } from "@/components/ui/heading";
 
 // deps
 import firestore from "@react-native-firebase/firestore";
@@ -14,6 +23,9 @@ const Workout = () => {
   const pathname = usePathname();
   const id = pathname.split("/")[2];
   const [name, setName] = useState("");
+
+  const [showAlertDialog, setShowAlertDialog] = useState(false);
+  const handleClose = () => setShowAlertDialog(false);
 
   const handleNameChange = async () => {
     try {
@@ -42,6 +54,7 @@ const Workout = () => {
 
   return (
     <View>
+      {/* Title */}
       <Input
         variant="outline"
         size="md"
@@ -55,8 +68,44 @@ const Workout = () => {
           onBlur={() => handleNameChange()}
         />
       </Input>
+
+      {/* Convert to allow the user to select the exercise and create a set */}
+      <>
+        <Button onPress={() => setShowAlertDialog(true)}>
+          <ButtonText>Open Dialog</ButtonText>
+        </Button>
+        <AlertDialog isOpen={showAlertDialog} onClose={handleClose} size="md">
+          <AlertDialogBackdrop />
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <Heading className="text-typography-950 font-semibold" size="md">
+                Are you sure you want to delete this post?
+              </Heading>
+            </AlertDialogHeader>
+            <AlertDialogBody className="mt-3 mb-4">
+              <Text>
+                Deleting the post will remove it permanently and cannot be
+                undone. Please confirm if you want to proceed.
+              </Text>
+            </AlertDialogBody>
+            <AlertDialogFooter className="">
+              <Button
+                variant="outline"
+                action="secondary"
+                onPress={handleClose}
+                size="sm"
+              >
+                <ButtonText>Cancel</ButtonText>
+              </Button>
+              <Button size="sm" onPress={handleClose}>
+                <ButtonText>Delete</ButtonText>
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </>
+
       {/* Need to add functionality to track a set, including selecting the exercise */}
-      <Text>{pathname}</Text>
       <Button
         size="md"
         variant="solid"
